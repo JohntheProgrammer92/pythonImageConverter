758c8e2f080bab271e68ed48ed86e39b5a8a7123
JohntheProgrammer92 <48172932+JohntheProgrammer92@users.noreply.github.com>
2020-09-26 15:30:16 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+	name='pyImgConvert',
+	version='1.0.0',
+	license="MIT",
+	description='Convert image types using CLI',
+	long_description=open('README.md').read(),
+	long_description_content_type='text/markdown',
+	author='http://github.com/Johntheprogrammer92',
+	url='http://github.com/Johntheprogrammer92/pyImgConvert',
+    packages=['pyImgconvert'],
+	py_modules=['setup'],
+    entry_points={
+		'console_scripts' : [
+			'pic=pyImgConvert.imgConvert:main'
+		]
+	}
+)
