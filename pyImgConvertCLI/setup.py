611848b1ba069e1fa8f6d6dd988376be78515e09
Johntheprogrammer92 <johnboner92@gmail.com>
2020-10-14 19:51:35 -0400

    added GUI(WIP)

---
+++ pyImgConvertCLI/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+	name='pythonImageConverter',
+	version='1.0.1',
+	license="MIT",
+	description='Convert image types using CLI',
+	long_description=open('README.md').read(),
+	long_description_content_type='text/markdown',
+	author='http://github.com/Johntheprogrammer92',
+	url='http://github.com/Johntheprogrammer92/pythonImageConverter',
+    install_requires=[
+		'pillow>=7.2.0'],
+    packages=['pythonImageConverter'],
+	
+    entry_points={
+		'console_scripts' : [
+			'pic=pythonImageConverter.pic:main'
+		]
+	}
+)
