ba383194f95d647f989ca5a32350659e69ac6d4d
Johntheprogrammer92 <johnboner92@gmail.com>
2020-10-20 20:00:55 -0400

    testing args

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='pythonImageConverter',
-	version='1.0.5',
+	version='1.0.7',
 	license="MIT",
 	description='Convert image types using CLI',
 	long_description=open('README.md').read(),
