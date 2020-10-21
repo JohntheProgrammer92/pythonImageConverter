9d251c59f72f9a9414bf6a355795f43c8d04cb9c
Johntheprogrammer92 <johnboner92@gmail.com>
2020-10-20 20:39:25 -0400

    fixing res folder

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='pythonImageConverter',
-	version='1.0.7.3',
+	version='1.0.7.4',
 	license="MIT",
 	description='Convert image types using CLI',
 	long_description=open('README.md').read(),
