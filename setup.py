0309884cf828ff1a757a8c325ede4120f14efe18
Johntheprogrammer92 <johnboner92@gmail.com>
2020-10-21 17:28:40 -0400

    fixed Readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='pythonImageConverter',
-	version='1.0.7.9',
+	version='1.0.8.0',
 	license="MIT",
 	description='Convert image types using CLI',
 	long_description=open('README.md').read(),
